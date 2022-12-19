605b16a2604d0ded408aa6c53a8e1d86eb1c8c6b
Jason Jurotich <jj@jjir.org>
2022-12-19 14:19:04 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -517,6 +517,7 @@ cmp.setup({
     { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
+    { name = "cmp_tabnine" },   
   }),
   formatting = {
     format = lspkind.cmp_format({
