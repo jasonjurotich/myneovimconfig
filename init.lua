4ec66ea81e9623dc2ea517c86cdbf3358a420d80
Jason Jurotich <jj@jjir.org>
2022-12-21 14:23:54 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -728,6 +728,9 @@ require("bufferline").setup({
 
 local custom_vscode = require("lualine.themes.vscode")
 custom_vscode.normal.c.bg = "None"
+custom_vscode.insert.c.bg = "None"
+custom_vscode.command.c.bg = "None"
+
 require("lualine").setup({
 	options = {
 		icons_enabled = false,
