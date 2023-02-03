b9da60dafe5e6696d9a983f36f551bdebc5d1e0a
Jason Jurotich <jj@jjir.org>
2023-02-02 19:10:41 -0600

    Update init2.lua

--- init2.lua
+++ init2.lua
@@ -309,19 +309,30 @@ local keymap = vim.keymap -- for conciseness
 local opts = { noremap = true, silent = true }
 
 keymap.set("i", "tt", "<ESC>")
-keymap.set("t", "J", "<C-\\><C-N><C-W><C-W>")
-keymap.set("t", "kj", "<C-\\><C-N>")
-keymap.set("t", "<C-j>", "<Cmd>wincmd j<CR>")
-keymap.set("t", "<C-k>", "<Cmd>wincmd k<CR>")
 
+-- terminal commands
+keymap.set("t", "<C-n>", "<C-\\><C-N><C-W><C-W>")
+keymap.set("t", "kj", "<C-\\><C-N>") -- escape edit mode in terminal
+keymap.set("t", "<C-j>", "<Cmd>wincmd j<CR>") -- move to terminal below
+keymap.set("t", "<C-k>", "<Cmd>wincmd k<CR>") -- move to terminal below
+keymap.set("t", "<C-h>", "<Cmd>wincmd h<CR>") -- move to terminal below
+keymap.set("t", "<C-l>", "<Cmd>wincmd l<CR>") -- move to terminal below
+
+-- keymap.set("t", "shift", "cmd", "k", "<Cmd>wincmd k<CR>") -- move to terminal above
+
+-- toggle term
+keymap.set("n", "te", ":ToggleTerm size=80 dir direction=vertical hidden=true <CR>")
+
+-- normal mode
 keymap.set("n", "Q", ":x<CR>")
 keymap.set("n", "W", ":x<CR>:x<CR>")
-keymap.set("n", "L", "<C-W><C-W>")
-keymap.set("n", "H", "<C-W><C-H>")
+
+keymap.set("n", "L", "<C-W><C-W>") -- move right in split windows
+keymap.set("n", "H", "<C-W><C-H>") -- move left in split windows
+
 keymap.set("n", "gb", "<C-o>")
 keymap.set("n", "ss", "ZZ")
 keymap.set("n", "vs", ":vs<CR>")
-keymap.set("n", "te", ":ToggleTerm size=80 dir direction=vertical hidden=true <CR>")
 keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 keymap.set("n", "<leader>db", ":w<CR>:Bdelete!<CR>")
@@ -332,6 +343,7 @@ keymap.set("n", "<leader>w", ":w<CR>")
 keymap.set("n", "<leader>5", "g_")
 keymap.set("n", "<leader>r", ":res +20<CR>")
 
+-- RESIZE WINDOWS
 keymap.set("n", "<S-Up>", ":resize -2<CR>", opts)
 keymap.set("n", "<S-Down>", ":resize +2<CR>", opts)
 keymap.set("n", "<S-Left>", ":vertical resize -2<CR>", opts)
@@ -341,6 +353,7 @@ keymap.set("n", "<S-Right>", ":vertical resize +2<CR>", opts)
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 
+-- tab management
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tu", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
@@ -349,7 +362,7 @@ keymap.set("n", "<leader>dt", ":tab DBUI<cr>", {})
 keymap.set("n", "<leader>du", ":tabclose<CR>:bnext<CR>:Bdelete!<CR><C-W><C-W>ZZ", {})
 
 -- vim-maximizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization only for buffers
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
@@ -373,7 +386,7 @@ keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current c
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
 
 -- goyo
-keymap.set("n", "<leader>gy", ":Goyo 80<CR>") -- mapping to restart lsp if necessary
+keymap.set("n", "<leader>gy", ":Goyo 80<CR>")
 
 vim.keymap.set("n", "<leader>s", function()
 	require("telescope.builtin").spell_suggest(require("telescope.themes").get_cursor({}))
